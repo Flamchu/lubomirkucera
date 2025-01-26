e1dd86e2be14ca44d961442a1b118211a4027978
Vochy <ndan@post.cz>
2025-01-26 21:19:14 +0100

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import Gallery from "./pages/Gallery";
+import Galerie from "./pages/Galerie";
+import Contact from "./pages/Contact";
+import "./App.scss"
+import Parez from "./assets/parez.jpg"
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="app-container">
+      
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/">Domů</Link>
+          </li>
+          <li>
+            <Link to="/galerie">Galerie</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="background">
+        <img src={Parez} alt="pařez" />
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/galerie" element={<Galerie />} />
+        <Route path="/kontakt" element={<Contact />} />
       </Routes>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../scss/Home.scss"
 import Lubos from "../assets/Lubos.png"
+import image1 from "../assets/wd1.webp"
+import image2 from "../assets/wd2.webp"
+import image3 from "../assets/wd3.webp"
+import image4 from "../assets/wd4.webp"
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <div className="slanted-section">
-        <img src={Lubos} alt="Lubomír Kučera" />
+    <div className="page-container">
+      <div className="intro">
+        
+        <div className="intro-text">
         <h1>Lubomír Kučera</h1>
+        <p>řezbář naivního umění</p>
+        </div>
+        <img src={Lubos} alt="Lubomír Kučera" />
       </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/gallery">Gallery</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="my-work">
+        <h1>Moje tvorba</h1>
+        <img src={image1} alt="" />
+        <img src={image2} alt="" />
+        <img src={image3} alt="" />
+        <img src={image4} alt="" />
+      </div>
     </div>
   );
 };
