74c6a785d52e24adbff53dd38265e260cd909b21
Vochy <ndan@post.cz>
2025-02-06 22:47:23 +0100

    contact facelift

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,6 +2,10 @@ import Footer from "../components/Footer";
 import React from "react";
 import "../scss/Contact.scss";
 import { Helmet } from "react-helmet-async";
+import image17 from "../assets/images/17.jpg"
+import email from "../assets/email.png"
+import phone from "../assets/telephone.png"
+import address from "../assets/location.png"
 
 const Contact: React.FC = () => {
 	return (
@@ -17,23 +21,30 @@ const Contact: React.FC = () => {
 					</h2>
 				</div>
 			</div>
+			<div className="contact-flexcont">	
 			<div className="contact-container">
-				<p>Líbí se Vám moje tvorba? Neváhejte mě kontaktovat!</p>
+				<h1>Kontaktujte mě</h1>
+				<p>Líbí se Vám moje tvorba? Napište mi mail nebo volejte!</p>
 
 				<div className="contact-details">
 					<div className="contact-item">
-						<strong>Telefon:</strong>
-						<a href="tel:+420732650305">+420 732 650 305</a>
+					<img src={phone} alt="" />
+					<a href="tel:+420732650305">+420 732 650 305</a>
 					</div>
 					<div className="contact-item">
-						<strong>Email:</strong>
+						<img src={email} alt="" />
 						<a href="mailto:luboskucera69@seznam.cz">luboskucera69@seznam.cz</a>
 					</div>
 					<div className="contact-item">
-						<strong>Adresa:</strong>
-						<span>Petrkov 5, 580 01 Havlíčkův Brod</span>
+					<img src={address} alt="" />
+					<a href="https://g.co/kgs/HgP34z9"> Petrkov 5, 580 01 Havlíčkův Brod</a>
 					</div>
 				</div>
+			</div>
+			<div className="divider"></div>
+			<div className="contact-container">
+			<img src={image17} alt="Obrázek" />
+			</div>
 			</div>
 			<Footer />
 		</>
